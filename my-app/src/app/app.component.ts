906e7674a15cf0d9e6669d4cf05ce63d4c88fd6c
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2022-01-18 18:07:53 +0200

    Censorship
    text field.
    censor button. Replacing text with asterisks

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -11,17 +11,22 @@ export class AppComponent {
   title = 'my-app';
   public bw_list = ''
   public bw_input = ''
+  public text_input = ''
   public inp_bw = 'Word here...'
+  public inp_text = 'Text here'
   public bad_words: any = []
 
   constructor() { }
 
-  clear(): void {
+  clear_bw() {
     this.inp_bw = ''
     this.bw_input = ''
   }
+  clear_text() {
+    this.inp_text = ''
+  }
 
-  addBtn(): void {
+  addBad(): void {
     let bw = this.inp_bw;
     if (bw !== '' && !bw.match(/\s/)) {
       this.bw_input = ''
@@ -37,6 +42,31 @@ export class AppComponent {
       this.bw_input = 'valid'
     }
   }
+
+  cenzor(): any {
+    let ti = this.inp_text
+
+    if (ti !== '') {
+      this.bad_words.forEach((word: { [Symbol.replace](string: string, replacer: (substring: string, ...args: any[]) => string): string; }) => {
+        ti = ti.replace(word, function (mach: any) {
+          let x = mach.length;
+          let y = '';
+          for (let i = 0; i < x; i++) {
+            y += "*";
+          }
+          return y;
+        });
+      })
+      this.inp_text = ti
+    }
+  }
+
+
+  reset() {
+    location.reload()
+  }
+
+
 
 
 }
