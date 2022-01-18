9dc753fca12e5843d8544ddb5401d4a4cb6639b4
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2022-01-18 16:53:07 +0200

    Adding words
    Adding words to an array and showing them in the list of bad words

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,9 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  public bad_words = []
+  public bw_list = ''
+  public bw_input = ''
+  public inp_bw = 'Word here...'
+  public bad_words: any = []
 
-  constructor(){
-    
+  constructor() { }
+
+  clear(): void {
+    this.inp_bw = ''
+    this.bw_input = ''
   }
+
+  addBtn(): void {
+    let bw = this.inp_bw;
+    if (bw !== '' && !bw.match(/\s/)) {
+      this.bw_input = ''
+      this.bad_words.push(bw)
+      this.inp_bw = 'Word here...'
+      this.bw_list = ''
+      for (let i = 0; i < this.bad_words.length; i++) {
+        this.bw_list += this.bad_words[i];
+        this.bw_list += ',  ';
+      }
+
+    } else {
+      this.bw_input = 'valid'
+    }
+  }
+
+
 }
+function removeClass(arg0: string): string {
+  throw new Error('Function not implemented.');
+}
+
